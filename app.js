6b066aae9ac2574e786da6b80655522e362e914e
rachelharu <rachel.haru9@gmail.com>
2021-09-30 14:51:25 -0700

    fixed mobile styling andmade the popup button appear with css instead of js

--- app.js
+++ app.js
@@ -1,42 +1,15 @@
-
 let btn = $('button');
 let socials = $("#popup");
 
-let theDiv = document.getElementById("btnshare");
-
-let content = document.createElement('button');
-content.setAttribute('class', 'circle');
-let img = document.createElement('img');
-img.setAttribute('src', 'images/icon-share.svg');
-
-
-
-btn.mouseover(function()
-{
-   $('.share').css("filter", "brightness(0) invert(1)" );
+btn.mouseover(function() {
+  $('.share').css("filter", "brightness(0) invert(1)");
 });
 
-btn.mouseout(function()
-{
+btn.mouseout(function() {
   $('.share').css("filter", "");
 });
 
 
-btn.click(function(){
-    socials.toggle();
+btn.click(function() {
+  socials.toggle();
 });
-
-
-content.click(function(){
-    socials.toggle();
-});
-
-
-if ($(window).width() < 650) {
-  theDiv.appendChild(content);
-  theDiv.appendChild(img);
-
-}
-else {
-   console.log('More than 960');
-}
